cadc82a949410fd06ca33a23182c1e9e513ae0df
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-29 09:10:35 +0530

    Added :check the frequency of paragraph

--- src/com/bridgelabz/HashTableMain.java
+++ src/com/bridgelabz/HashTableMain.java
@@ -2,11 +2,11 @@ package com.bridgelabz;
 
 public class HashTableMain {
     public static void main(String[] args) {
-        String sentence = "To be or not to be";
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
         HashTable<String, Integer> hashTable = new HashTable<>(6);
 
-        String[] wordsArray = sentence.split(" ");
+        String[] wordsArray = paragraph.split(" ");
         for (String word : wordsArray) {
             Integer currentFrequency = hashTable.get(word);
             if (currentFrequency == null)
